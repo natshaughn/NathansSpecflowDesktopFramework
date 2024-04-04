07be5230f5e9030443d716dedb4b69a39d4918f8
Nathan Shaughnessy <nathan.shaughnessy@roq.co.uk>
2024-04-04 23:23:02 +0100

    dRIVER AND hOOKS ADDED, FIRST TETS PASSING, SECOND TEST PASSING UP UNTIL FILE EXPLORER OPENING

--- Tests/StepDefinitions/FileExplorerSteps.cs
+++ Tests/StepDefinitions/FileExplorerSteps.cs
@@ -1,12 +1,27 @@
+using NathansSpecflowDesktopFramework.Drivers;
+using NUnit.Framework;
+using OpenQA.Selenium.Appium.Windows;
+
 namespace NathansSpecflowDesktopFramework.Tests.StepDefinitions
 {
     [Binding]
     public class FileExplorerSteps
     {
+        // Obtain the driver instance from the DriverManager
+        WindowsDriver<WindowsElement> sessionWord = DriverManager.GetDriver();
+
         [Then(@"the document will be created and saved in the desktop directory")]
         public void ThenTheDocumentWillBeCreatedAndSavedInTheDesktopDirectory()
         {
-            throw new PendingStepException();
+            // NEEDS WORK HERE IT'S NOT CLICKING FILE EXPLORER 
+            sessionWord.FindElementByName("File Explorer").Click();
+            sessionWord.FindElementByName("Downloads (pinned)").Click();
+            // Get the random file name generated in the previous step
+            string randomFileName = FileMenuSteps.GetRandomFileName();
+
+            sessionWord.FindElementByName(randomFileName);
+            // Verify that the file exists
+            Assert.IsTrue(File.Exists(randomFileName), "File was not saved");
         }
 
         [Then(@"a PDF file will be created in the desktop directory")]
